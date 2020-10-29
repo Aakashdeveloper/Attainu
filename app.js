cb5f9dbe0cdbbba6a099e1500b66896234e996b5
Aakashdeveloper <ahanda205@gmail.com>
2020-10-30 01:56:42 +0530

    a

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const port = process.env.PORT || 5001;
 import AuthController from './auth/AuthContorl';
 
 app.use(cors());
+//html
+app.set('views','./src/views');
+//view engine
+app.set('view engine','ejs');
 
 app.get('/',(req,res) => {
-    res.status(200).send('Health Check Pass')
+    res.render('home')
 })
 
 app.use('/api/auth', AuthController);
